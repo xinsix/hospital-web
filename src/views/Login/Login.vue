8dc88a75f55ccc952a4a1dee6c148876a2969012
xinsix <xinsix@aliyun.com>
2021-12-30 20:02:30 +0800

    更新了登录后跳转的问题

--- src/views/Login/Login.vue
+++ src/views/Login/Login.vue
@@ -128,7 +128,6 @@ export default {
             data:{phone:phone,sms:captcha},
             success:function(data){
                 if (data=="success") {
-                    alert("验证码正确！");
                     window.location.href="http://yy.xinsix.com/home";
                 }else
                     alert("验证码错误！")
